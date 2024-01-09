950528a4f066f1e9f589d5a90342b79395233910
Julie kim <juliekimdev@gmail.com>
2024-01-09 10:10:18 +0900

    Update 짝수의 합.js

--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.js
@@ -1,3 +1,16 @@
+// 1
+function solution(n) {
+    let answer = 0;
+    for ( let i = 0; i <= n; i++) {
+        if ( i % 2 === 0) {
+            answer += i;
+        }
+    }
+    return answer;
+}
+
+
+// 2
 function solution(n) {
     let answer = 0;
     for ( i = 2; i <= n; i += 2) {
